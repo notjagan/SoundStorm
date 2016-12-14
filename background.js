1bbfcf656d3ccc0e3647dcab38f60975e39bc444
Jagan Prem <notjagan@gmail.com>
2016-12-14 00:22:26 -0500

    Create functionality for constructing passive control bar

--- background.js
+++ background.js
@@ -1,57 +1,85 @@
-var visible = {};
-var port;
+var activePorts = {};
+var soundcloudPort;
+var activeId;
+var interval;
+
+function initiateConnection() {
+	soundcloudPort.postMessage({ action: "get-html" });
+	interval = setInterval(function () {
+		soundcloudPort.postMessage({ action: "get-html" });
+	}, 100);
+}
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+	if (activePorts[activeId]) {
+		clearInterval(interval);
+	}
+	activeId = activeInfo.tabId;
+	if (activePorts[activeId]) {
+		initiateConnection();
+	}
+});
 
 chrome.commands.onCommand.addListener(function (command) {
 	if (command === "toggle-play-control") {
-		chrome.tabs.query({
-			active: true,
-			currentWindow: true
-		}, function (tabs) {
-			if (tabs.length > 0) {
-				var activeId = tabs[0].id;
-				visible[activeId] = !visible[activeId];
-				if (visible[activeId]) {
-					if (typeof port === "undefined") {
-						chrome.tabs.query({ url: "*://soundcloud.com/*" }, function (tabs) {
+		chrome.tabs.get(activeId, function (tab) {
+			if (!tab.url.match(/https?:\/\/(www\.)?soundcloud\.[a-z]+\b.*/)) {
+				if (!activePorts[activeId]) {
+					activePorts[activeId] = chrome.tabs.connect(activeId, { name: String(activeId) });
+					activePorts[activeId].onDisconnect.addListener(function (port) {
+						console.log(port.name);
+						if (port.name === String(activeId)) {
+							clearInterval(interval);
+							delete activePorts[activeId];
+						}
+						delete port;
+					});
+					if (typeof soundcloudPort === "undefined") {
+						chrome.tabs.query({
+							url: "*://soundcloud.com/*",
+							audible: true
+						}, function (tabs) {
 							if (tabs.length > 0) {
 								var soundcloudId = tabs[0].id;
-								port = chrome.tabs.connect(soundcloudId, { name: "soundcloud-port" });
-								port.onMessage.addListener(function (msg) {
+								soundcloudPort = chrome.tabs.connect(soundcloudId, { name: "soundcloud-port" });					
+								soundcloudPort.onMessage.addListener(function (msg) {
 									if (msg.connected) {
-										port.postMessage({
-											action: "get-settings",
-											createBar: true
-										});
-									}
-									else {
-										chrome.tabs.sendMessage(activeId, {
-											action: "apply-settings",
-											settings: msg.settings,
-											createBar: msg.createBar
+										initiateConnection();
+									} else if (msg.html) {
+										activePorts[activeId].postMessage({
+											action: "apply-html",
+											html: msg.html
 										});
 									}
 								});
-								port.postMessage({ action: "handshake" });
-								chrome.tabs.onRemoved.addListener(function (soundcloudId) {
-									port = undefined;
+								chrome.tabs.onRemoved.addListener(function (tabId) {
+									if (tabId === soundcloudId) {
+										soundcloudPort = undefined;
+										clearInterval(interval);
+									} else if (activePorts[tabId]) {
+										activePorts[tabId].disconnect();
+										if (tabId === activeId) {
+											clearInterval(interval);
+										}
+										delete activePorts[tabId];
+									}
 								});
-							}
-							else {
-								chrome.tabs.sendMessage(activeId, {
+							} else {
+								activePorts[activeId].postMessage({
 									action: "report-error",
-									error: "soundcloud-closed",
-									createBar: true
+									error: "soundcloud-closed"
 								});
 								return;
 							}
 						});
+					} else {
+						initiateConnection();
 					}
-					else {
-						port.postMessage({
-							action: "get-settings",
-							createBar: true
-						});
-					}
+				} else {
+					activePorts[activeId].postMessage({ action: "remove-bar" });
+					activePorts[activeId].disconnect();
+					clearInterval(interval);
+					delete activePorts[activeId];
 				}
 			}
 		});
--- content.js
+++ content.js
@@ -1,31 +1,43 @@
-chrome.runtime.onConnect.addListener(function (port) {
-	console.assert(port.name === "soundcloud-port");
-	port.onMessage.addListener(function (msg) {
-		switch (msg.action) {
-			case "handshake":
-				port.postMessage({ connected: true });
-				break;
-			case "get-settings":
-				port.postMessage({
-					settings: {
-						
-					},
-					createBar: msg.createBar
-				});
-				break;
-			default:
-				throw new Error("Unrecognized action");
-		}
-	});
-});
+var active = false;
+var playControl;
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	switch (request.action) {
-		case "apply-settings":
-			break;
-		case "report-error":
-			break;
-		default:
-			throw new Error("Unrecognized action");
+chrome.runtime.onConnect.addListener(function (port) {
+	if (port.name === "soundcloud-port") {
+		port.postMessage({ connected: true });
+		port.onMessage.addListener(function (msg) {
+			if (msg.action === "get-html") {
+				console.log("Capturing html");
+				if (!active) {
+					playControl = $("div.playControls")[0];
+					active = true;
+				}
+				var html = playControl.outerHTML;
+				port.postMessage({ html: html });
+			}
+		});
+	} else {
+		port.onMessage.addListener(function (msg) {
+			switch (msg.action) {
+				case "apply-html":
+					if (!active) {
+						$.get(chrome.extension.getURL("/playControl.html"), function (data) {
+							playControl = $(data);
+							playControl.append($(msg.html));
+							playControl.appendTo("body");
+						})
+						active = true;
+					} else {
+						$("div.playControls")[0].outerHTML = msg.html;
+					}
+					break;
+				case "remove-bar":
+					playControl.remove();
+					active = false;
+					break;
+				case "report-error":
+					console.log(msg.error);
+					break;
+			}
+		});
 	}
 });
